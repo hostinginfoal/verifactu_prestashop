27d929fe1e69438ce59dd06aad84af8e15a07013
root <root@webserver13.infoal.com>
2025-10-10 09:46:45 +0200

    1.3.4

--- verifactu.php
+++ verifactu.php
@@ -55,7 +55,7 @@ class Verifactu extends Module
     {
         $this->name = 'verifactu';
         $this->tab = 'billing_invoicing';
-        $this->version = '1.3.3';
+        $this->version = '1.3.4';
         $this->author = 'InFoAL S.L.';
         $this->need_instance = 0;
         $this->is_configurable = true;
