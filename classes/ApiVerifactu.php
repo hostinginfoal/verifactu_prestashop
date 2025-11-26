c87f1382e01dfde4dbeeda155138aea42d5c0d5a
root <root@webserver13.infoal.com>
2025-11-26 09:55:21 +0100

    1.4.8

--- classes/ApiVerifactu.php
+++ classes/ApiVerifactu.php
@@ -1,28 +1,29 @@
 <?php
 /**
-* InFoAL S.L.
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to hosting@infoal.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    InFoAL S.L. <hosting@infoal.com>
-*  @copyright InFoAL S.L.
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of InFoAL S.L.
-*/
+ * InFoAL S.L.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to a Commercial License (EULA)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is strictly prohibited to redistribute, copy, modify, or resell
+ * this code without the written permission of InFoAL S.L.
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to hosting@infoal.com so we can send you a copy immediately.
+ *
+ * @author    InFoAL S.L. <hosting@infoal.com>
+ * @copyright 2025 InFoAL S.L.
+ * @license   Proprietary - All Rights Reserved
+ * @category  Compliance
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ */
 
 namespace Verifactu\VerifactuClasses;
 
